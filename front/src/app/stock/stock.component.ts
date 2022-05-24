74c68fb60c7dc19cb80fbb2c8bdd51f8f4bad8dd
Xavier ROUSSELOT <xavier.rousselot@aviva.com>
2022-05-24 09:29:09 +0200

    dev: articles added

--- front/src/app/stock/stock.component.ts
+++ front/src/app/stock/stock.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Article } from '../interfaces/article';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
-  styleUrls: ['./stock.component.scss']
+  styleUrls: ['./stock.component.scss'],
 })
 export class StockComponent implements OnInit {
+  articles: Article[] = [
+    { name: 'Marteau', price: 2.99, qty: 15 },
+    { name: 'Faucille', price: 1.99, qty: 12 },
+    { name: 'Scie', price: 3.99, qty: 15 },
+    { name: 'Perceuse', price: 9.99, qty: 18 },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
