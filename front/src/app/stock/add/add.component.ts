2d35bd0333ed4840666591ca8d8accc44c36e447
Xavier ROUSSELOT <xavier.rousselot@aviva.com>
2022-05-24 10:28:36 +0200

    dev: validation form added
    Librairie ReactiveFormModule ajoutée

--- front/src/app/stock/add/add.component.ts
+++ front/src/app/stock/add/add.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  styleUrls: ['./add.component.scss'],
 })
 export class AddComponent implements OnInit {
+  formGroup = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    price: new FormControl(0, [Validators.required, Validators.min(0)]),
+    qty: new FormControl(0, [Validators.required, Validators.min(0)]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
