4e4ddf760d568c8f2086e937af042d57d8e1b111
Xavier ROUSSELOT <xavier.rousselot@aviva.com>
2022-05-25 15:32:26 +0200

    test: directive autofocus

--- front/src/app/widgets/autofocus.directive.spec.ts
+++ front/src/app/widgets/autofocus.directive.spec.ts
@@ -1,8 +1,43 @@
+import { Component } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { AutofocusDirective } from './autofocus.directive';
 
+@Component({
+  selector: 'app-test',
+  template: '<input appAutoFocus />',
+})
+export class TestComponent {}
+
+@Component({
+  selector: 'app-test-with-select',
+  template: '<input appAutoFocus="select" class="test-with-select" />',
+})
+export class InputWithSelectComponent {}
+
 describe('AutofocusDirective', () => {
-  it('should create an instance', () => {
-    //const directive = new AutofocusDirective();
-    //expect(directive).toBeTruthy();
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        InputWithSelectComponent,
+        AutofocusDirective,
+      ],
+    }).compileComponents();
+  });
+
+  it('should autofocus', () => {
+    const fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(document.activeElement).toBe(compiled.querySelector('input'));
+  });
+
+  it('should selected in input with select', () => {
+    const fixture = TestBed.createComponent(InputWithSelectComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(document.activeElement).toBe(
+      compiled.querySelector('input.test-with-select')
+    );
   });
 });
